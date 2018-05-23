4f6f9af9a019b5535b7478790b0ecca55d758c03
Suchitra Naik <susanaik@gmail.com>
2018-05-23 19:57:18 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,12 +2,5 @@
 # It should inherit from ActiveRecord::Base
 
 class Costume < ActiveRecord::Base
-  def change
-    create_table :Costume do |c|
-      c.string :name
-      c.float :price
-      c.integer :size
-      c.string :image_url
-    end
-  end
+  
 end 
